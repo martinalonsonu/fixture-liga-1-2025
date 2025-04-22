ab1ed5399a7dacc7eeaeb87809ec2c9c8bc2a044
Martin Nuñez Navarro <m.nunez@rpalatam.com.pe>
2025-04-22 12:03:03 -0500

    feat: ultimos cambios fixture femenino

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,45 @@
-import React from 'react'
-import { urlAssets, epigraph } from '../utils/constants';
+import React from "react";
+import { urlAssets, epigraph } from "../utils/constants";
 
 const classes = {
-    container: 'header',
-    liga1: 'header__first-icon',
-    title: 'header__title',
-    subtitle: 'header__second-icon',
-    epigraph: 'header__epigraph'
-}
+  container: "header",
+  liga1: "header__first-icon",
+  title: "header__title",
+  subtitle: "header__second-icon",
+  epigraph: "header__epigraph",
+};
 
 const Header: React.FC = () => {
-    const images = {
-        liga1: { src: `${urlAssets}/img/liga-femenina.png`, alt: 'Liga Femenina' },
-        extra: { src: `${urlAssets}/img/apertura-clausura.png`, alt: 'Apertura y Clausura - Liga Femenina' }
-    }
-    return (
-        <header className={classes.container}>
-            <img src={images.liga1.src} alt={images.liga1.alt} width={210} height={98} className={classes.liga1} />
-            <h1 className={classes.title}>FIXTURE</h1>
-            <img src={images.extra.src} alt={images.extra.alt} width={377} height={92} className={classes.subtitle} />
-            <p className={classes.epigraph}>{epigraph}</p>
-        </header>
-    )
-}
+  const images = {
+    liga1: {
+      src: `${urlAssets}/img/liga-femenina.png?d=2`,
+      alt: "Liga Femenina",
+    },
+    extra: {
+      src: `${urlAssets}/img/apertura-clausura.png`,
+      alt: "Apertura y Clausura - Liga Femenina",
+    },
+  };
+  return (
+    <header className={classes.container}>
+      <img
+        src={images.liga1.src}
+        alt={images.liga1.alt}
+        width={210}
+        height={98}
+        className={classes.liga1}
+      />
+      <h1 className={classes.title}>FIXTURE</h1>
+      <img
+        src={images.extra.src}
+        alt={images.extra.alt}
+        width={377}
+        height={92}
+        className={classes.subtitle}
+      />
+      <p className={classes.epigraph}>{epigraph}</p>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,33 +1,35 @@
 const brand = "El Comercio";
-const twitterAccount = "elcomercio_peru";    
+const twitterAccount = "elcomercio_peru";
 const urlBrand = "https://elcomercio.pe";
-const logo = 'logo-depor-white';
+const logo = "logo-depor-white";
 const urlPortadaEspeciales = "https://elcomercio.pe";
 const domainMarca = "https://especiales.elcomercio.pe/?q=especiales/";
-const domainAssets = "https://d1ts5g4ys243sh.cloudfront.net/proyectos_especiales_prod/especiales/";
+const domainAssets =
+  "https://d1ts5g4ys243sh.cloudfront.net/proyectos_especiales_prod/especiales/";
 const urlCommonAssets = `${domainAssets}elementos-comunes/`;
 
-const titleEspecial = 'Fixture Liga Femenina 2025: Programación del Torneo Apertura y Clausura de Alianza Lima, Universitario, Sporting Cristal y otros clubes';
-const slugEspecial = 'liga-femenina-peru-2025';
+const titleEspecial =
+  "Fixture Liga Femenina 2025: Programación del Torneo Apertura y Clausura de Alianza Lima, Universitario, Sporting Cristal y otros clubes";
+const slugEspecial = "liga-femenina-peru-2025";
 const urlEspecial = `${domainMarca}${slugEspecial}/index.html`;
 const urlAssets = `${domainAssets}${slugEspecial}`;
-const urlClubIcons = `${urlAssets}/img/logos`
-const epigraph = "A partir de este año, la Liga Femenina FPF implementará un formato que dividirá el campeonato en Torneo Apertura y Clausura, ambos con un formato de todos contra todos. En cada torneo, los seis primeros equipos de la tabla avanzarán a los play-offs. Los dos primeros lugares se clasificarán directamente a las semifinales, mientras que los equipos entre el tercer y el sexto lugar competirán en los cuartos de final bajo un formato eliminatorio: 3º contra 6º y 4º contra 5º. Estas rondas se jugarán en una sede única para garantizar imparcialidad. La culminación del torneo será un enfrentamiento a doble partido, con una final de ida y vuelta para determinar al campeón. Por otro lado, los equipos que ocupen los 2 últimos lugares de la Tabla Acumulada descenderán a la Liga de Ascenso Femenina 2026.";
-
+const urlClubIcons = `${urlAssets}/img/logos`;
+const epigraph =
+  "La Liga Femenina se dividirá en un Apertura, Clausura y fases finales. En cada torneo, los 6 primeros avanzarán a los playoffs: los 2 primeros se clasificarán a las 'semis', mientras que los equipos entre el tercer y sexto lugar competirán en cuartos: 3º vs. 6º y 4º vs. 5º. La culminación del torneo será un enfrentamiento a doble partido, para determinar al campeón. Los que ocupen los dos últimos lugares del Acumulado descenderán a la Liga de Ascenso 2026.";
 
 export {
-    brand, 
-    twitterAccount, 
-    urlBrand, 
-    logo, 
-    urlPortadaEspeciales, 
-    domainMarca, 
-    domainAssets, 
-    urlCommonAssets, 
-    titleEspecial, 
-    slugEspecial, 
-    urlEspecial, 
-    urlAssets, 
-    urlClubIcons,
-    epigraph
-}
+  brand,
+  twitterAccount,
+  urlBrand,
+  logo,
+  urlPortadaEspeciales,
+  domainMarca,
+  domainAssets,
+  urlCommonAssets,
+  titleEspecial,
+  slugEspecial,
+  urlEspecial,
+  urlAssets,
+  urlClubIcons,
+  epigraph,
+};
